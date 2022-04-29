43f53a8ac8e91f6599eae90f0fb7f733c5b70397
morealva <emilcealvarez10@gmail.com>
2022-04-29 10:14:38 -0300

    proyecto1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LinkComponent } from './components/link/link.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LinkComponent
   ],
   imports: [
     BrowserModule,
