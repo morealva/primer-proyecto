e39abafef3868aa49435703e5963c9e1b2a861b8
morealva <emilcealvarez10@gmail.com>
2022-04-29 11:43:13 -0300

    cambios 29/04

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LinkComponent } from './components/link/link.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home',component:HomeComponent},
+  {path: '',component:HomeComponent},
+  {path: 'link',component:LinkComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LinkComponent } from './components/link/link.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LinkComponent
+    LinkComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
